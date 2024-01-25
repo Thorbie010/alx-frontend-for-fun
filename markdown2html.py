52819e96effb926d0f8a0e482df8333bc58519cd
Jonah <JonahEmmanuel54@gmail.com>
2024-01-25 13:51:26 +0100

    fixing issues

--- markdown2html.py
+++ markdown2html.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
 
     if __name__ == "__main__":
         if len(sys.argv) != 3:
-            print("Usage: ./markdown2html.py <input_file> <output_file>", file=sys.stderr)
+            print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
             sys.exit(1)
 
         input_file = sys.argv[1]
